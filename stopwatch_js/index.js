014f84369c2267f771b87034bcde4e21e5b31f67
OlgaVasileva <polubaryeva.olga@gmail.com>
2022-09-11 21:04:21 -0400

    Stopwatch program

---
+++ stopwatch_js/index.js
@@ -0,0 +1,56 @@
+let timeDisplay = document.getElementById("timeDisplay");
+let startBtn = document.getElementById("startBtn");
+let pauseBtn = document.getElementById("pauseBtn");
+let resetBtn = document.getElementById("resetBtn");
+
+let elapsedTime = 0;
+let startTime = 0;
+let intervalId;
+let paused = true;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+
+startBtn.addEventListener("click", () => {
+    if (paused) {
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(updateTime, 1000);
+    }
+});
+
+pauseBtn.addEventListener("click", () => {
+    if(!paused){
+        paused = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalId);
+    }
+ });
+
+resetBtn.addEventListener("click", () => {
+    paused = true;
+    clearInterval(intervalId);
+    startTime = 0;
+    elapsedTime = 0;
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    timeDisplay.textContent = "00:00:00";
+});
+
+function updateTime(){
+    elapsedTime = Date.now() - startTime;
+    secs = Math.floor((elapsedTime / 1000) % 60);
+    mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+    secs = pad(secs);
+    mins = pad(mins);
+    hrs = pad(hrs);
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+    
+    function pad(unit){
+        return String(unit).length > 1 ? unit : "0" + unit;
+       // return (("0") + unit).length > 2 ? unit : "0" + unit;
+    }
+
+}
